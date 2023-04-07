8229896587014cc2a42e841f00dd3623792b7d85
yueyv <yueyvlunhui@gmail.com>
2023-04-07 19:10:55 +0800

    first

--- src/components/firstCard.vue
+++ src/components/firstCard.vue
@@ -1,32 +1,38 @@
 <script setup lang='ts'>
 import { message } from 'ant-design-vue';
-import { ref,reactive} from 'vue'
-const param="1"
+import { ref, reactive } from 'vue'
+interface Props {
+    img: number,
+    title?: string,
+    content?: string
+}
+const props = withDefaults(defineProps<Props>(), {
+    img: 0,
+    title: '不存在',
+    content: '不存在'
+})
 
-function getImageUrl(param:string) {
+function getImageUrl(param: number) {
     return new URL(`../assets/images/${param}.jpg`, import.meta.url).href;
 }
 </script>
 
 <template>
-
     <div class="container">
         <div class="card">
             <div class="img-box">
-                <img :src=getImageUrl(param) alt="">
+                <img :src=getImageUrl(img) alt="">
             </div>
             <div class="text-box">
-                <h2>卡片一</h2>
-                <p>我是内容一我是内容一我是内容一我是内容一我是内容一我是内容一我是内容一我是内容一我是内容一我是内容一我是内容一我是内容一</p>
+                <h2>{{ title }} {{ img }}</h2>
+                <p>{{ content }}</p>
             </div>
         </div>
     </div>
-
 </template>
 
 <style scoped lang='less'>
-
-.container{
+.container {
     /* 相对定位 */
     position: relative;
     /* 弹性布局 */
@@ -35,10 +41,11 @@ function getImageUrl(param:string) {
     align-items: center;
     /* 允许换行 */
     flex-wrap: wrap;
-    padding: 30px;
-    flex:1
+    // padding: 30px;
+    flex: 1
 }
-.container .card{
+
+.container .card {
     position: relative;
     max-width: 300px;
     height: 215px;
@@ -47,30 +54,41 @@ function getImageUrl(param:string) {
     padding: 20px 15px;
     border-radius: 5px;
     /* 阴影 */
-    box-shadow: 0 5px 200px rgba(0,0,0,0.5);
+    box-shadow: 0 5px 200px rgba(0, 0, 0, 0.5);
     /* 动画过渡 */
     transition: 0.3s ease-in-out;
 }
-.container .card:hover{
-    height: 420px;
+
+.container .card:hover {
+    height: 320px;
 }
-.container .card .img-box{
+
+.container .card .img-box {
     position: relative;
-    width: 260px;
+    // width: 260px;
     height: 260px;
     border-radius: 5px;
     /* 溢出隐藏 */
     overflow: hidden;
-     top: -60px;
-     left: 4px;
+    top: -60px;
     /* 阴影 */
-    box-shadow: 0 5px 20px rgba(0,0,0,0.2);
+    box-shadow: 0 5px 20px rgba(0, 0, 0, 0.2);
     z-index: 1;
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
 }
-.container .card .img-box img{
+
+.container .card .img-box {
     width: 100%;
+    // height: 100%;
+    // object-fit: fill;
 }
-.container .card .text-box{
+
+.container .card .text-box {
     position: relative;
     margin-top: -140px;
     padding: 10px 15px;
@@ -82,14 +100,16 @@ function getImageUrl(param:string) {
     opacity: 0;
     transition: 0.3s ease-in-out;
 }
-.container .card .text-box p{
+
+.container .card .text-box p {
     text-align: left;
     line-height: 25px;
     margin-top: 10px;
     font-size: 15px;
     color: #555;
 }
-.container .card:hover .text-box{
+
+.container .card:hover .text-box {
     /* 鼠标移入，设置元素可见 */
     visibility: visible;
     opacity: 1;
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,27 +1,41 @@
 <script setup lang='ts'>
-import { ref,reactive} from 'vue'
+import { ref, reactive } from 'vue'
 import card from "../components/firstCard.vue"
+// const emit=defineEmits<>('nav')
+const index = ref(0)
+const lists = ref([1, 2, 3, 4, 5, 6, 7, 8, 9])
+
 </script>
 
 <template>
-    <div id="home">
-        <card></card>
-        <card></card>
-        <card></card>
-
+    <div class="main">
+        <div id="card">
+            <card v-for="list in lists" :img="list" :key="list"></card>
+        </div>
     </div>
-
 </template>
 
 <style scoped lang='less'>
-#home{
-    /* 100%窗口高度 */
+.main {
+    width: 100vw;
     height: 100vh;
+    background: linear-gradient(200deg, #80d0c7, #13547a);
+}
+
+#card {
+    /* 100%窗口高度 */
     /* 弹性布局 水平垂直居中 */
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 300px);
+
+    // justify-content: center;
+    // align-items: center;
     /* 渐变背景 */
-    background: linear-gradient(200deg,#80d0c7,#13547a);
+    width: 80vw;
+    height: 60vh;
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
 }
 </style>
