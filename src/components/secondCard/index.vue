1b980b9383678d6bb79ce60db8d85ee183dd9276
yueyv <yueyvlunhui@gmail.com>
2023-04-25 16:54:57 +0800

    update

--- src/components/secondCard/index.vue
+++ src/components/secondCard/index.vue
@@ -7,9 +7,9 @@ const link_list=[[{
     "link_image":'https://static.runoob.com/images/favicon.ico',
     "link_title":'菜鸟教程'
 }],[{
-    "link":'https://www.runoob.com/',
-    "link_image":'https://static.runoob.com/images/favicon.ico',
-    "link_title":'菜鸟教程'
+    "link":'https://juejin.cn/',
+    "link_image":'https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web//static/favicons/favicon-32x32.png',
+    "link_title":'掘金'
 }]]
 
 
@@ -28,10 +28,10 @@ const link_list=[[{
     <section>
         <div class="second_main">
             <div class="a_title">               
-                <fieldset>教程</fieldset> 
+                <fieldset>社区</fieldset> 
             </div>
-            <second_a v-for="key in link_list[0].length"  :link="link_list[0][key-1].link" :link_img="link_list[0][key-1].link_image"
-                :link_title="link_list[0][key-1].link_title"></second_a>   
+            <second_a v-for="key in link_list[1].length"  :link="link_list[1][key-1].link" :link_img="link_list[1][key-1].link_image"
+                :link_title="link_list[1][key-1].link_title"></second_a>   
         </div>
     </section>
 </template>
@@ -43,7 +43,9 @@ const link_list=[[{
     background-color: rgba(255, 255, 255, 0.1);
     // opacity: 0.2;
     border-radius: 20px;
-    padding: 10px;
+    padding: 0px;
+    padding-left: 20px;
+    padding-right: 20px;
     // top: 20vw;
     margin-bottom: 20px;
     // grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
@@ -75,7 +77,7 @@ fieldset {
     font-size: 24px;
     color: #ffd7d7;
     // width: 80vw;
-    padding: 0 5%;
+    padding: 0 ;
     grid-column: 1/-1;
-    padding-bottom: 4px;
+    // padding-bottom: 4px;
 }</style>
