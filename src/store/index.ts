e70f8b83238398d5c71c9de57b6dcf9451d0ebb7
yueyv <yueyvlunhui@gmail.com>
2023-05-29 22:09:41 +0800

    update

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,15 +2,13 @@ import { defineStore } from "pinia";
 import { Names } from "./store-name";
 type User = {
     name: string,
-    age: number
 }
 
 const Login = (): Promise<User> => {
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve({
-                name: "挥挥",
-                age: 99
+                name: "test",
             })
         }, 10);
     })
@@ -25,11 +23,9 @@ export const useTestStore = defineStore(Names.TEST, {
     //computed 计算修饰
     getters: {
         newName():string{
-            return `$-${this.name}-${this.getUserAge}`
+            return `$-${this.name}`
         },
-        getUserAge():number{
-            return this.user.age
-        }
+
     },
     //mothods 做同步和异步 
     actions: {
